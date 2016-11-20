8527605ce2b42f9a1d5504290fa239bdad4667d2
Jherez Taylor <jherez.taylor@gmail.com>
2016-11-21 02:07:33 +0800

    Language distribution - intial methods to fetch the distribution of languages in the collection

--- run.py
+++ run.py
@@ -3,9 +3,21 @@ import os
 import glob
 import pprint
 import csv
+import pymongo
 
-json_path = "HatebaseData/json/"
-csv_path = "HatebaseData/csv/"
+JSON_PATH = "HatebaseData/json/"
+CSV_PATH = "HatebaseData/csv/"
+DB_URL = "mongodb://140.114.79.146:27017"
+
+
+def connect():
+    """Return connection object for Mongo DB"""
+    try:
+        conn = pymongo.MongoClient(DB_URL)
+        print "Connected to DB at " + DB_URL + " successfully"
+    except pymongo.errors.ConnectionFailure, ex:
+        print "Could not connect to MongoDB: %s" % ex
+    return conn
 
 
 def get_filenames():
@@ -13,10 +25,10 @@ def get_filenames():
     Reads all the json files in the folder and removes the drive and path and
     extension, only returning a list of strings with the file names.
     """
-    file_path = glob.glob(json_path + "*.json")
+    file_path = glob.glob(JSON_PATH + "*.json")
     result = []
-    for f in file_path:
-        drive, path = os.path.splitdrive(f)
+    for entry in file_path:
+        drive, path = os.path.splitdrive(entry)
         path, filename = os.path.split(path)
         name = os.path.splitext(filename)[0]
         result.append(str(name))
@@ -29,12 +41,12 @@ def load_json_file(filename):
     """
     result = []
     try:
-        with open(json_path + filename + '.json', 'r') as f:
-            result = json.load(f)
-    except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        with open(JSON_PATH + filename + '.json', 'r') as entry:
+            result = json.load(entry)
+    except IOError as ex:
+        print "I/O error({0}): {1}".format(ex.errno, ex.strerror)
     else:
-        f.closed
+        entry.close()
         return result
 
 
@@ -42,7 +54,7 @@ def write_to_csv(filename, result):
     """
     Writes a list to csv with the given filename
     """
-    output = open(csv_path + filename + '.csv', 'wb')
+    output = open(CSV_PATH + filename + '.csv', 'wb')
     writer = csv.writer(output, quoting=csv.QUOTE_ALL, lineterminator='\n')
     for val in result:
         writer.writerow([val])
@@ -55,8 +67,8 @@ def extract_corpus(file_list):
     Loads a set of json files and builds a corpus from the
     terms within
     """
-    for f in file_list:
-        json_data = load_json_file(f)
+    for entry in file_list:
+        json_data = load_json_file(entry)
         result = []
 
         data = json_data['data']['datapoint']
@@ -64,12 +76,15 @@ def extract_corpus(file_list):
 
         for entry in data:
             result.append(str(entry['vocabulary']))
-        write_to_csv(f, result)
+        write_to_csv(entry, result)
 
 
-def count_sightings(json):
+def count_sightings(json_obj):
+    """
+    Returns a count of the number of sightings per word in corpus
+    """
     try:
-        return int(json['number_of_sightings'])
+        return int(json_obj['number_of_sightings'])
     except KeyError:
         return 0
 
@@ -79,35 +94,111 @@ def load_csv_file(filename):
     Accepts a file name and loads it as a list
     """
     try:
-        with open(csv_path + filename + '.csv', 'r') as f:
-            reader = csv.reader(f)
+        with open(CSV_PATH + filename + '.csv', 'r') as entry:
+            reader = csv.reader(entry)
             temp = list(reader)
             # flatten to 1D, it gets loaded as 2D array
             result = [x for sublist in temp for x in sublist]
-    except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except IOError as ex:
+        print "I/O error({0}): {1}".format(ex.errno, ex.strerror)
     else:
-        f.closed
+        entry.close()
         return result
 
 
 def count_entries(file_list):
+    """
+    Performs a count of the number of number of words in the corpus
+    """
     result = []
-    for f in file_list:
-        with open(csv_path + f + '.csv', "r") as entry:
+    for obj in file_list:
+        with open(CSV_PATH + obj + '.csv', "r") as entry:
             reader = csv.reader(entry, delimiter=",")
             col_count = len(reader.next())
-            res = {"Filename": f, "Count": col_count}
+            res = {"Filename": obj, "Count": col_count}
             result.append(res)
     return result
 
 
-def main():
+def build_query_string(query_words):
+    """
+    Builds an OR concatenated string for querying the Twitter Search API
+    """
+    result = ''.join(
+        [q + ' OR ' for q in query_words[0:(len(query_words) - 1)]])
+    return result + str(query_words[len(query_words) - 1])
+
+
+def test_file_operations():
+    """
+    Test previous methods
+    """
     file_list = get_filenames()
     extract_corpus(file_list)
     num_entries = count_entries(file_list)
-    # pprint.pprint(num_entries)
-    print load_csv_file('about_sexual_orientation_eng_pg1')
+    pprint.pprint(num_entries)
+    res = load_csv_file('about_sexual_orientation_eng_pg1')
+    res2 = build_query_string(res)
+    print res2
+
+
+def unicode_to_utf(unicode_list):
+    """
+    Converts a list of strings from unicode to utf8
+    """
+    return [x.encode('UTF8') for x in unicode_list]
+
+
+def get_language_list(client, db_name):
+    """
+    Returns a list of all the matching languages within the collection
+    """
+    dbo = client[db_name]
+    distinct_lang = dbo.tweets.distinct("lang")
+    return unicode_to_utf(distinct_lang)
+
+
+def get_language_distribution(client, db_name, lang_list):
+    """
+    Returns the distribution of tweets matching either
+    english, undefined or spanish
+    """
+    dbo = client[db_name]
+    pipeline = [
+        {"$match": {"lang": {"$in": lang_list}}},
+        {"$group": {"_id": "$lang", "count": {"$sum": 1}}},
+        {"$project": {"language": "$_id", "count": 1, "_id": 0}}
+    ]
+    return dbo.tweets.aggregate(pipeline)
+
+
+def test_get_language_distribution(client):
+    """
+    Test and print results of aggregation
+    """
+    lang_list = get_language_list(client, 'twitter')
+    cursor = get_language_distribution(client, 'twitter', lang_list)
+    for document in cursor:
+        print document
+
+
+def test_get_language_subset(client):
+    """
+    Test and print the results of aggregation
+    Constrains language list to en, und, es
+    """
+    lang_list = ['en', 'und', 'es']
+    cursor = get_language_distribution(client, 'twitter', lang_list)
+    for document in cursor:
+        print document
+
+
+def main():
+    """
+    Test functionality
+    """
+    client = connect()
+    test_get_language_subset(client)
 
 if __name__ == '__main__':
     main()
