a4865be6452018a6c9595aa03122dc4e167c31ce
Jherez Taylor <jherez.taylor@gmail.com>
2016-12-07 13:54:13 +0800

    Testing new keyword_search method. Failing

--- modules/db/mongomethods.py
+++ modules/db/mongomethods.py
@@ -11,6 +11,7 @@ from langid.langid import LanguageIdentifier, model
 from pymongo import MongoClient
 from pymongo import errors
 from pymongo import UpdateOne
+from pymongo import InsertOne
 from bson.son import SON
 from bson.code import Code
 from bson.objectid import ObjectId
@@ -350,7 +351,7 @@ def parse_undefined_lang(client, db_name, subset, lang):
 
 
 @fileops.do_cprofile
-def keyword_search(client, db_name, lang_list, keywords):
+def keyword_search(client, db_name, keywords):
     """Perform a text search with the provided keywords in batches of 10.
     Outputs value to new collection
 
@@ -362,16 +363,27 @@ def keyword_search(client, db_name, lang_list, keywords):
     """
 
     # Split the incoming list of keywords into lists of size 10
-    decomposed_keywords = [keywords[i:i + 3]
-                           for i in xrange(0, len(keywords), 3)]
+    # decomposed_keywords = [keywords[i:i + 10]
+    #                        for i in xrange(0, len(keywords), 10)]
 
     dbo = client[db_name]
-    for item in decomposed_keywords:
-        search_query = ' '.join(item)
+    operations = []
+    for search_query in keywords:
+        # search_query = ' '.join(item)
+        print search_query
         pipeline = [
             {"$match": {"$text": {"$search": search_query}}},
-            {"$project": {"_id": 1, 'text': 1, 'id': 1, 'timestamp': 1, 'entities': 1, 'retweeted': 1,
-                          'coordinates': 1, 'lang': 1, 'user.id': 1, 'user.screen_name': 1, 'user.location': 1}},
-            {"$out": "subset_search_test"}
+            {"$project": {"_id": 1, 'text': 1, 'id': 1, 'timestamp': 1, 'retweeted': 1,
+                          'lang': 1, 'user.id_str': 1, 'user.screen_name': 1, 'user.location': 1}}
         ]
-        dbo.tweets.aggregate(pipeline, allowDiskUse=True)
+        cursor = dbo.tweets.aggregate(pipeline, allowDiskUse=True)
+        for document in cursor:
+            operations.append(InsertOne(document))
+
+         # Send once every 1000 in batch
+        if len(operations) == 1000:
+            dbo['keywords'].bulk_write(operations, ordered=False)
+            operations = []
+
+    if len(operations) > 0:
+        dbo['keywords'].bulk_write(operations, ordered=False)
