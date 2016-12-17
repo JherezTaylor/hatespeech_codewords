c1a8699833129cb92b0e3935b07aad6b284f149e
Jherez Taylor <jherez.taylor@gmail.com>
2016-12-17 17:28:01 +0800

    Fixes #13

--- hatespeech_core/modules/db/mongo_methods.py
+++ hatespeech_core/modules/db/mongo_methods.py
@@ -25,7 +25,7 @@ def connect(db_url=None):
     Returns:
         pymongo.MongoClient: Connection object for Mongo DB_URL
     """
-    max_sev_sel_delay = 20
+    max_sev_sel_delay = 500
     if db_url is None:
         db_url = settings.DB_URL
     try:
