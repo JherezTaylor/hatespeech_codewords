8c9914bf6dabb41314c3cbf67f0e10a44cc9d0e2
Jherez Taylor <jherez.taylor@gmail.com>
2017-02-24 16:33:45 +0800

    Send lists to be checked as pipeline args

--- hatespeech_core/modules/preprocessing/candidate_selection.py
+++ hatespeech_core/modules/preprocessing/candidate_selection.py
@@ -13,27 +13,21 @@ from ..db import mongo_base
 from ..db import mongo_search_pipelines
 
 
-def run_select_hs_candidates(connection_params):
-    """ Start the HS indentification pipeline
-    """
-
-    args2 = ["candidates_hs_exp5_19_Feb", False, False]
-    time1 = file_ops.time()
-    mongo_search_pipelines.select_hs_candidates(connection_params, args2)
-    time2 = file_ops.time()
-    time_diff = (time2 - time1) * 1000.0
-
-    print("%s function took %0.3f ms" % ("select_hs_candidates", time_diff))
-    send_notification = file_ops.send_job_notification(
-        settings.MONGO_SOURCE + ": HS select candidates took " + str(time_diff) + " ms", "Complete")
-    print(send_notification.content)
-
-
 def run_select_porn_candidates(connection_params):
     """ Start the Porn indentification pipeline
     """
 
-    args2 = ["candidates_porn_exp4_14_Feb", False, False]
+    # Load keywords once and avoid redudant disk reads
+    # Load our blacklist and filter any tweet that has a matching keyword
+    porn_black_list = set(file_ops.read_csv_file(
+        "porn_blacklist", settings.WORDLIST_PATH))
+
+    hs_keywords = set(file_ops.read_csv_file("hate_1", settings.TWITTER_SEARCH_PATH) +
+                      file_ops.read_csv_file("hate_2", settings.TWITTER_SEARCH_PATH) +
+                      file_ops.read_csv_file("hate_3", settings.TWITTER_SEARCH_PATH))
+
+    args2 = ["candidates_porn_exp4_14_Feb", False,
+             False, porn_black_list, hs_keywords]
     time1 = file_ops.time()
     mongo_search_pipelines.select_porn_candidates(connection_params, args2)
     time2 = file_ops.time()
@@ -41,7 +35,36 @@ def run_select_porn_candidates(connection_params):
 
     print("%s function took %0.3f ms" % ("select_porn_candidates", time_diff))
     send_notification = file_ops.send_job_notification(
-        settings.MONGO_SOURCE + ": Porn select candidates took " + str(time_diff) + " ms", "Complete")
+        settings.MONGO_SOURCE + ": Porn Candidates took " + str(time_diff) + " ms", "Complete")
+    print(send_notification.content)
+
+
+def run_select_hs_candidates(connection_params):
+    """ Start the HS indentification pipeline
+    """
+
+    # Load keywords once and avoid redudant disk reads
+    # Load our blacklist and filter any tweet that has a matching keyword
+    porn_black_list = set(file_ops.read_csv_file("porn_bigrams", settings.WORDLIST_PATH) +
+                          file_ops.read_csv_file("porn_trigrams", settings.WORDLIST_PATH) +
+                          file_ops.read_csv_file("porn_quadgrams", settings.WORDLIST_PATH))
+
+    hs_keywords = set(file_ops.read_csv_file("hate_1", settings.TWITTER_SEARCH_PATH) +
+                      file_ops.read_csv_file("hate_2", settings.TWITTER_SEARCH_PATH) +
+                      file_ops.read_csv_file("hate_3", settings.TWITTER_SEARCH_PATH))
+    black_list = set(file_ops.read_csv_file(
+        "blacklist", settings.WORDLIST_PATH))
+
+    args2 = ["candidates_hs_exp5_19_Feb", False, False,
+             porn_black_list, hs_keywords, black_list]
+    time1 = file_ops.time()
+    mongo_search_pipelines.select_hs_candidates(connection_params, args2)
+    time2 = file_ops.time()
+    time_diff = (time2 - time1) * 1000.0
+
+    print("%s function took %0.3f ms" % ("select_hs_candidates", time_diff))
+    send_notification = file_ops.send_job_notification(
+        settings.MONGO_SOURCE + ": HS Candidates took " + str(time_diff) + " ms", "Complete")
     print(send_notification.content)
 
 
@@ -49,7 +72,20 @@ def run_select_general_candidates(connection_params):
     """ Start the General indentification pipeline
     """
 
-    args2 = ["candidates_gen_exp3_garb_15_Jan"]
+    # Load keywords once and avoid redudant disk reads
+    # Load our blacklist and filter any tweet that has a matching keyword
+
+    porn_black_list = dict.fromkeys(file_ops.read_csv_file(
+        "porn_blacklist", settings.WORDLIST_PATH))
+
+    hs_keywords = dict.fromkeys(file_ops.read_csv_file("hate_1", settings.TWITTER_SEARCH_PATH) +
+                                file_ops.read_csv_file("hate_2", settings.TWITTER_SEARCH_PATH) +
+                                file_ops.read_csv_file("hate_3", settings.TWITTER_SEARCH_PATH))
+    black_list = dict.fromkeys(file_ops.read_csv_file(
+        "blacklist", settings.WORDLIST_PATH))
+
+    args2 = ["candidates_gen_exp3_15_Jan", False,
+             False, porn_black_list, hs_keywords, black_list]
     time1 = file_ops.time()
     mongo_search_pipelines.select_general_candidates(connection_params, args2)
     time2 = file_ops.time()
@@ -57,7 +93,7 @@ def run_select_general_candidates(connection_params):
 
     print("%s function took %0.3f ms" % ("select_gen_candidates", time_diff))
     send_notification = file_ops.send_job_notification(
-        settings.MONGO_SOURCE + ": General select candidates took " + str(time_diff) + " ms", "Complete")
+        settings.MONGO_SOURCE + ": General Candidates took " + str(time_diff) + " ms", "Complete")
     print(send_notification.content)
 
 
