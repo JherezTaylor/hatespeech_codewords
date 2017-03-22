a2caf2df83da56bb99552bebca3c4322f062bb38
Jherez Taylor <jherez.taylor@gmail.com>
2017-03-22 21:08:21 +0800

    Added spacy nlp pipeline - Uses multithreading with pipe - Tests pass

--- hatespeech_core/core.py
+++ hatespeech_core/core.py
@@ -8,6 +8,7 @@ Core module
 
 from modules.preprocessing import db_cleaning
 from modules.preprocessing import candidate_selection
+from modules.preprocessing import classifier_concept
 from modules.utils import twitter_api
 
 
@@ -16,8 +17,9 @@ def main():
     Run operations
     """
     # db_cleaning.preprocessing_pipeline()
-    candidate_selection.sentiment_pipeline()
+    # candidate_selection.sentiment_pipeline()
     # db_cleaning.run_update_missing_text()
+    classifier_concept.start_job()
 
 if __name__ == "__main__":
     main()
