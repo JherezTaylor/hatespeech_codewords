8741c1abac230bdd5eac2ca04e286acaae37da14
Jherez Taylor <jherez.taylor@gmail.com>
2017-03-23 17:33:32 +0800

    Testing nlp pipe with db cursor

--- hatespeech_core/modules/preprocessing/classifier_concept.py
+++ hatespeech_core/modules/preprocessing/classifier_concept.py
@@ -6,9 +6,11 @@
 This module serves as a proof of concept for the classifier model
 """
 
+import itertools
 import spacy
 import joblib
 from ..utils import settings
+from ..db import mongo_base
 from ..utils.CustomTwokenizer import CustomTwokenizer
 from ..pattern_classifier import SimpleClassifier, PatternVectorizer
 
@@ -34,7 +36,7 @@ def load_emotion_classifier():
     pv_persistence = settings.MODEL_PATH + "pattern_vectorizer.pkl.compressed"
     _cls = joblib.load(cls_persistence)
     _pv = joblib.load(pv_persistence)
-    return [_cls, _pv]
+    return (_cls, _pv)
 
 
 def extract_lexical_features_test(nlp, tweet_list):
@@ -48,10 +50,9 @@ def extract_lexical_features_test(nlp, tweet_list):
         print(doc)
         for parsed_doc in doc:
             result.append((parsed_doc.orth_, parsed_doc.tag_))
-    # print(result)
 
 
-def feature_extraction_pipeline(connection_params, nlp, tweet_generator_obj):
+def feature_extraction_pipeline(connection_params, nlp):
     """Handles the extraction of features needed for the model.
     Inserts parsed documents to database.
     Args:
@@ -62,15 +63,41 @@ def feature_extraction_pipeline(connection_params, nlp, tweet_generator_obj):
         tweet_generator_obj: (generator): MongoDB cursor or file loaded documents.
     """
 
-    db_name = connection_params[0]
-    collection = connection_params[1]
+    client = mongo_base.connect()
+    connection_params.insert(0, client)
+    _cls, _pv = load_emotion_classifier()
 
-    for doc in nlp.pipe(tweet_generator_obj, batch_size=10000, n_threads=3):
-        pass
+    query = {}
+    query["filter"] = {"text": {"$ne": None}}
+    query["projection"] = {"text": 1}
+    query["limit"] = 0
+    query["skip"] = 0
+    query["no_cursor_timeout"] = True
+
+    cursor = mongo_base.finder(connection_params, query, False)
+
+    # Makes a copy of the MongoDB cursor, to the best of my
+    # knowledge this does not attempt to exhaust the cursor
+    cursor_1, cursor_2 = itertools.tee(cursor)
+    object_ids = (object_id["_id"] for object_id in cursor_1)
+    tweet_texts = (tweet_text["text"] for tweet_text in cursor_2)
+
+    docs = nlp.pipe(tweet_texts, batch_size=10000, n_threads=3)
+    for object_id, doc in zip(object_ids, docs):
+        print(object_id, doc.text)
+        # _emotion_vec = _pv.transform([doc.text])
+        # doc["emotion_coverage"] = _cls.get_top_classes(_emotion_vec, ascending=True, n=2)
+        # doc["min_emotion"] = _cls.get_max_score_class(_emotion_vec)
 
 
 def start_job():
+    connection_params = ["twitter", "test_suite"]
     nlp = init_nlp_pipeline()
-    tweet_list = ["I'm here :) :D 99", "get rekt",
-                  "lol hi", "just a prank bro", "#squadgoals okay"]
-    extract_lexical_features_test(nlp, tweet_list)
+    tweet = {}
+    tweet["text"] = "I'm here :) :D 99"
+    tweet["id"] = 7849
+    # tweet_list = ["I'm here :) :D 99", "get rekt",
+    #               "lol hi", "just a prank bro", "#squadgoals okay"]
+    # extract_lexical_features_test(nlp, tweet_list)
+    feature_extraction_pipeline(connection_params, nlp)
+    
