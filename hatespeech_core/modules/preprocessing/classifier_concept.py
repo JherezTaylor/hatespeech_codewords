1c988fd6e6d32ba5047ca5a06d6180732cfc3425
Jherez Taylor <jherez.taylor@gmail.com>
2017-03-24 21:50:12 +0800

    Feature extraction pipeline complete - Open to optimizations - Fixes #31 Fixes #45

--- hatespeech_core/modules/preprocessing/classifier_concept.py
+++ hatespeech_core/modules/preprocessing/classifier_concept.py
@@ -9,8 +9,10 @@ This module serves as a proof of concept for the classifier model
 import itertools
 import spacy
 import joblib
+from pymongo import InsertOne
 from ..utils import settings
 from ..utils import file_ops
+from ..utils import notifiers
 from ..utils import text_preprocessing
 from ..db import mongo_base
 from ..utils.CustomTwokenizer import CustomTwokenizer
@@ -53,7 +55,7 @@ def extract_lexical_features_test(nlp, tweet_list):
         for parsed_doc in doc:
             result.append((parsed_doc.orth_, parsed_doc.tag_))
 
-
+@notifiers.do_cprofile
 def feature_extraction_pipeline(connection_params, nlp):
     """Handles the extraction of features needed for the model.
     Inserts parsed documents to database.
@@ -61,13 +63,22 @@ def feature_extraction_pipeline(connection_params, nlp):
         connection_params  (list): Contains connection objects and params as follows:
             0: db_name     (str): Name of database to query.
             1: collection  (str): Name of collection to use.
+            2: target_collection (str): Name of output collection.
         nlp  (spaCy model): Language processing pipeline
         tweet_generator_obj: (generator): MongoDB cursor or file loaded documents.
     """
 
     client = mongo_base.connect()
+    db_name = connection_params[0]
+    target_collection = connection_params[2]
     connection_params.insert(0, client)
     _cls, _pv = load_emotion_classifier()
+
+    # Setup client object for bulk op
+    bulk_client = mongo_base.connect()
+    dbo = bulk_client[db_name]
+    dbo.authenticate(settings.MONGO_USER, settings.MONGO_PW,
+                     source=settings.DB_AUTH_SOURCE)
 
     query = {}
     query["filter"] = {"text": {"$ne": None}}
@@ -77,7 +88,7 @@ def feature_extraction_pipeline(connection_params, nlp):
     query["no_cursor_timeout"] = True
     cursor = mongo_base.finder(connection_params, query, False)
 
-    HS_KEYWORDS = set(file_ops.read_csv_file("hate_1", settings.TWITTER_SEARCH_PATH) +
+    hs_keywords = set(file_ops.read_csv_file("hate_1", settings.TWITTER_SEARCH_PATH) +
                       file_ops.read_csv_file("hate_2", settings.TWITTER_SEARCH_PATH) +
                       file_ops.read_csv_file("hate_3", settings.TWITTER_SEARCH_PATH))
 
@@ -87,14 +98,15 @@ def feature_extraction_pipeline(connection_params, nlp):
     object_ids = (object_id["_id"] for object_id in cursor_1)
     tweet_texts = (tweet_text["text"] for tweet_text in cursor_2)
 
-    staging = []
     operations = []
+    count = 0
     # Carry out the same stage/bulk operation
     # https://github.com/explosion/spaCy/issues/172
     docs = nlp.pipe(tweet_texts, batch_size=10000, n_threads=3)
     for object_id, doc in zip(object_ids, docs):
         _emotion_vec = _pv.transform([doc.text])
-
+        count += 1
+        print("count ", count)
         # Construct a new tweet object to be appended
         parsed_tweet = {}
         parsed_tweet["_id"] = object_id
@@ -102,30 +114,56 @@ def feature_extraction_pipeline(connection_params, nlp):
         parsed_tweet["emotion"] = _cls.get_top_classes(
             _emotion_vec, ascending=True, n=2)
         parsed_tweet["min_emotion"] = _cls.get_max_score_class(_emotion_vec)
-        parsed_tweet["dependencies"] = [{"text": token.lower_, "lemma": token.lemma_, "pos": token.tag_, "dependency": token.dep_, "root": token.head.lower_} for token in doc if not(token.is_punct)]
-        parsed_tweet["noun_chunks"] = [{"text": np.lower_,"root": np.root.head.lower_} for np in doc.noun_chunks]
-        parsed_tweet["brown_cluster_ids"] = [token.cluster for token in doc if token.cluster != 0]
-        parsed_tweet["tokens"] = [token for token in doc if not(token.is_stop or token.is_punct or token.lower_ == "rt" or token.is_digit or token.prefix_ == "#")]
-        parsed_tweet["hs_keyword_matches"] = set(parsed_tweet["tokens"]).intersection(HS_KEYWORDS)
-        parsed_tweet["hs_keyword_count"] = len(parsed_tweet["hs_keyword_matches"])
-        parsed_tweet["hs_keywords"] = True if parsed_tweet["hs_keyword_count"] > 0 else False
-        parsed_tweet["related_keywords"] = [[w.lower_ for w in text_preprocessing.get_similar_words(nlp.vocab[token], settings.NUM_SYNONYMS)] for token in text_preprocessing.get_keywords(doc)]
-        parsed_tweet["unknown_words"] = [token.lower_ for token in doc if token.is_oov and token.prefix_ != "#"]
-        parsed_tweet["unknown_words_count"] = len(parsed_tweet["unknown_words"])
+        parsed_tweet["dependencies"] = [{"text": token.lower_, "lemma": token.lemma_, "pos": token.tag_,
+                                         "dependency": token.dep_, "root": token.head.lower_} for token in doc if not(token.is_punct)]
+        parsed_tweet["noun_chunks"] = [
+            {"text": np.text.lower(), "root": np.root.head.text.lower()} for np in doc.noun_chunks]
+        parsed_tweet["brown_cluster_ids"] = [
+            token.cluster for token in doc if token.cluster != 0]
+        parsed_tweet["tokens"] = [token for token in doc if not(
+            token.is_stop or token.is_punct or token.lower_ == "rt" or token.is_digit or token.prefix_ == "#")]
+        parsed_tweet["hs_keyword_matches"] = list(set(
+            parsed_tweet["tokens"]).intersection(hs_keywords))
+        parsed_tweet["hs_keyword_count"] = len(
+            parsed_tweet["hs_keyword_matches"])
+        parsed_tweet["hs_keywords"] = True if parsed_tweet[
+            "hs_keyword_count"] > 0 else False
+        # parsed_tweet["related_keywords"] = [[w.lower_ for w in text_preprocessing.get_similar_words(
+        #     nlp.vocab[token], settings.NUM_SYNONYMS)] for token in text_preprocessing.get_keywords(doc)]
+        parsed_tweet["unknown_words"] = [
+            token.lower_ for token in doc if token.is_oov and token.prefix_ != "#"]
+        parsed_tweet["unknown_words_count"] = len(
+            parsed_tweet["unknown_words"])
         parsed_tweet["comment_length"] = len(doc)
-        parsed_tweet["avg_token_length"] = round(sum(len(token) for token in doc) / len(doc), 0)
-        parsed_tweet["uppercase_token_count"] = text_preprocessing.count_uppercase_tokens(doc)
-        parsed_tweet["bigrams"] = text_preprocessing.create_ngrams(doc.text.split(), 2)
-        parsed_tweet["trigrams"] = text_preprocessing.create_ngrams(doc.text.split(), 3)
-        parsed_tweet["char_trigrams"] = text_preprocessing.create_character_ngrams(doc.text.split(), 3)
-        parsed_tweet["char_quadgrams"] = text_preprocessing.create_character_ngrams(doc.text.split(), 4)
-        parsed_tweet["char_pentagrams"] = text_preprocessing.create_character_ngrams(doc.text.split(), 5)
-        parsed_tweet["hashtags"] = [token for token in doc if token.prefix_ == "#"]
-        print(object_id, doc.text)
+        parsed_tweet["avg_token_length"] = round(
+            sum(len(token) for token in doc) / len(doc), 0) if len(doc) > 0 else 0
+        parsed_tweet[
+            "uppercase_token_count"] = text_preprocessing.count_uppercase_tokens(doc)
+        parsed_tweet["bigrams"] = text_preprocessing.create_ngrams(
+            doc.text.split(), 2)
+        parsed_tweet["trigrams"] = text_preprocessing.create_ngrams(
+            doc.text.split(), 3)
+        parsed_tweet["char_trigrams"] = text_preprocessing.create_character_ngrams(
+            doc.text.split(), 3)
+        parsed_tweet["char_quadgrams"] = text_preprocessing.create_character_ngrams(
+            doc.text.split(), 4)
+        parsed_tweet["char_pentagrams"] = text_preprocessing.create_character_ngrams(
+            doc.text.split(), 5)
+        parsed_tweet["hashtags"] = [
+            token for token in doc if token.prefix_ == "#"]
+        operations.append(InsertOne(parsed_tweet))
+
+        if len(operations) == settings.BULK_BATCH_SIZE:
+            _ = mongo_base.do_bulk_op(dbo, target_collection, operations)
+            operations = []
+        # print(object_id, doc.text)
+    if operations:
+        _ = mongo_base.do_bulk_op(dbo, target_collection, operations)
 
 
 def start_job():
-    connection_params = ["twitter", "test_suite"]
+    """Run operations"""
+    connection_params = ["twitter", "test_suite", "feature_test"]
     nlp = init_nlp_pipeline()
     tweet = {}
     tweet["text"] = "I'm here :) :D 99"
