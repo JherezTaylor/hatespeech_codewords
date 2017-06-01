324d9a09114235a345cfc29594844b8eedd083a8
Jherez Taylor <jherez.taylor@gmail.com>
2017-06-01 15:12:22 +0800

    Fix for file path

--- hatespeech_core/core.py
+++ hatespeech_core/core.py
@@ -20,7 +20,7 @@ def main():
     # feature_prep.start_feature_extraction()
     # db_cleaning.preprocessing_pipeline()
     # feature_prep.start_store_preprocessed_text()
-    neural_embeddings.train_embeddings()
+    # neural_embeddings.train_embeddings()
     neural_embeddings.train_fasttext_classifier()
 
 
