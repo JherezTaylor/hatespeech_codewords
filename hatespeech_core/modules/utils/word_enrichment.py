cebab179f0b60b102abeab7cb245b6c3bef201a2
Jherez Taylor <jherez.taylor@gmail.com>
2017-06-22 23:09:21 +0800

    Fixes #116 - Candidate check now uses p@k and max(unbiased[token], biased[token]) - Modified several functions to use kwargs - Calculate and store idf scores

--- hatespeech_core/modules/utils/word_enrichment.py
+++ hatespeech_core/modules/utils/word_enrichment.py
@@ -9,15 +9,18 @@ contextual representation of words.
 
 import numpy as np
 from . import settings
-from . import notifiers
 
 
 def gensim_top_k_similar(model, row, field_name, k):
     """ Returns the top k similar word vectors from a word embedding model.
-    Args:
+    Args
+    ----
         model (gensim.models): Gensim word embedding model.
+
         row (pandas.Dataframe): Row extracted from a dataframe.
+
         field_name (str): Name of dataframe column to extract.
+
         k (int): Number of results to return.
     """
 
@@ -33,130 +36,216 @@ def gensim_top_k_similar(model, row, field_name, k):
 
 def spacy_top_k_similar(word, k):
     """ Returns the top k similar word vectors from a spacy embedding model.
-    Args:
+    Args
+    ----
         word (spacy.token): Gensim word embedding model.
+
         k (int): Number of results to return.
     """
-    queries = [w for w in word.vocab if not (word.is_oov or word.is_punct or word.like_num or word.is_stop or word.lower_ == "rt")
-               and w.has_vector and w.lower_ != word.lower_ and w.is_lower == word.is_lower and w.prob >= -15]
+    queries = [w for w in word.vocab if not
+               (word.is_oov or word.is_punct or word.like_num or
+                word.is_stop or word.lower_ == "rt")
+               and w.has_vector and w.lower_ != word.lower_
+               and w.is_lower == word.is_lower and w.prob >= -15]
+
     by_similarity = sorted(
         queries, key=lambda w: word.similarity(w), reverse=True)
     cosine_score = [word.similarity(w) for w in by_similarity]
     return by_similarity[:k], cosine_score[:k]
 
 
-def select_candidate_codewords(biased_embeddings, unbiased_embeddings, vocab_pair, hs_keywords, hs_check=True, topn=5, hs_threshold=1):
+def select_candidate_codewords(**kwargs):
     """ Select words that share a similarity (functional) or relatedness with a known
     hate speech word. Similarity and relatedness are depenedent on the model passed.
     The idea is to trim the passed vocab.
-    Args:
+    Args
+    ----
 
         biased_embeddings (list): Stores (gensim.models): Gensim word embedding model
+
             0: dep2vec_embedding
             1: word_embedding (either fasttext or w2v)
+
         unbiased_embeddings (list): Stores (gensim.models): Gensim word embedding model
             0: base_dep2vec_embedding
             1: base_word_embedding (either fasttext or w2v)
-        vocab_pair (list):
-            0: biased_vocab (dict): Dictionary of token:probability values. Calculated
+
+        freq_vocab_pair (list):
+            0: biased_vocab_freq (dict): Dictionary of token:frequency values. Calculated
                              on the number of documents where that token appears.
-            1: unbiased_vocab (dict)
-        topn (int): Number of words to check against in the embedding model.
-        hs_threshold (int): Number of HS keyword matches that need to appear in the topn results.
+            1: unbiased_vocab_freq (dict)
+
+        idf_vocab_pair (list):
+            0: biased_vocab_idf (dict): Dictionary of token:idf values. Calculated
+                             on the number of documents where that token appears.
+            1: unbiased_vocab_idf (dict)
+
+        topn (int): Number of words to check against in the embedding model. default=5.
+
+        p_at_k_threshold (float): Threshold for P@k calculation on the number of
+                HS keyword matches that need to appear in the topn, default=0.2.
 
     Returns:
          dict: dictionary of token:probabiliy pairs.
     """
 
-    dep2vec_embedding = biased_embeddings[0]
-    word_embedding = biased_embeddings[1]
+    dep2vec_embedding = kwargs["biased_embeddings"][0]
 
-    base_dep2vec_embedding = unbiased_embeddings[0]
-    base_word_embedding = unbiased_embeddings[1]
+    biased_vocab_freq = kwargs["freq_vocab_pair"][0]
+    unbiased_vocab_freq = kwargs["freq_vocab_pair"][1]
 
-    biased_vocab = vocab_pair[0]
-    unbiased_vocab = vocab_pair[1]
+    kwargs["topn"] = 5 if "topn" not in kwargs else kwargs["topn"]
+    kwargs["p_at_k_threshold"] = 0.2 if "p_at_k_threshold" not in kwargs else kwargs[
+        "p_at_k_threshold"]
 
     candidate_codewords = {}
     dep2vec_embedding_vocab = set(dep2vec_embedding.index2word)
-    for token in biased_vocab:
+    for token in biased_vocab_freq:
         if token in dep2vec_embedding_vocab:
             contextual_representation = get_contextual_representation(
-                token, [dep2vec_embedding, word_embedding], [base_dep2vec_embedding, base_word_embedding], topn=topn)
+                token=token, biased_embeddings=kwargs["biased_embeddings"],
+                unbiased_embeddings=kwargs["unbiased_embeddings"], topn=kwargs["topn"])
 
-            if hs_check:
-                check_intersection = set(
+            if kwargs["hs_check"]:
+                similar_word_set = set(
                     [entry[0] for entry in contextual_representation["similar_words"]])
 
-                diff = hs_keywords.intersection(check_intersection)
-                if len(diff) >= hs_threshold:
-                    settings.logger.debug("Token: %s | Set: %s", token, diff)
+                intersection = kwargs[
+                    "hs_keywords"].intersection(similar_word_set)
+                p_at_k = round(float(len(intersection)) /
+                               float(kwargs["topn"]), 3)
+
+                freq_compare = unbiased_vocab_freq[
+                    token] if token in unbiased_vocab_freq else 0
+                if p_at_k >= kwargs["p_at_k_threshold"] and \
+                        biased_vocab_freq[token] > freq_compare:
+
+                    # TODO check to see if we can do an AND / OR operation with
+                    # p@k for related words
+                    settings.logger.debug(
+                        "Token: %s | Set: %s", token, similar_word_set)
 
                     candidate_codewords[token] = prep_code_word_representation(
-                        token, contextual_representation, biased_vocab, unbiased_vocab, hs_keywords)
+                        token=token, contextual_representation=contextual_representation,
+                        freq_vocab_pair=kwargs[
+                            "freq_vocab_pair"], topn=kwargs["topn"],
+                        idf_vocab_pair=kwargs["idf_vocab_pair"],
+                        hs_keywords=kwargs["hs_keywords"])
             else:
                 check_intersection = set(
                     [entry[0] for entry in contextual_representation["similar_words"]])
 
-                diff = hs_keywords.intersection(check_intersection)
+                diff = kwargs["hs_keywords"].intersection(check_intersection)
                 if not diff:
                     candidate_codewords[token] = prep_code_word_representation(
-                        token, contextual_representation, biased_vocab, unbiased_vocab, hs_keywords)
+                        token=token, contextual_representation=contextual_representation,
+                        freq_vocab_pair=kwargs[
+                            "freq_vocab_pair"], topn=kwargs["topn"],
+                        idf_vocab_pair=kwargs["idf_vocab_pair"],
+                        hs_keywords=kwargs["hs_keywords"])
 
     return candidate_codewords
 
 
-def prep_code_word_representation(token, contextual_representation, biased_vocab, unbiased_vocab, hs_keywords):
+def prep_code_word_representation(**kwargs):
     """ Helper function for preparing the dictionary fields.
+    Args
+    ----
+    token
+    contextual_representation
+    freq_vocab_pair
+    idf_vocab_pair
+    hs_keywords
+    topn
     """
 
+    token = kwargs["token"]
+    similar_words = kwargs["contextual_representation"]["similar_words"]
+    related_words = kwargs["contextual_representation"]["related_words"]
+    similar_words_unbiased = kwargs[
+        "contextual_representation"]["similar_words_unbiased"]
+    related_words_unbiased = kwargs[
+        "contextual_representation"]["related_words_unbiased"]
+
+    hs_keywords = kwargs["hs_keywords"]
+    topn = kwargs["topn"]
+
+    biased_vocab_freq = kwargs["freq_vocab_pair"][0]
+    unbiased_vocab_freq = kwargs["freq_vocab_pair"][1]
+
+    biased_vocab_idf = kwargs["idf_vocab_pair"][0]
+    unbiased_vocab_idf = kwargs["idf_vocab_pair"][1]
+
     hs_similar_words = [
-        word for word in contextual_representation["similar_words"] if word[0] in hs_keywords]
+        word for word in similar_words if word[0] in hs_keywords]
 
     other_similar_words = [
-        word for word in contextual_representation["similar_words"] if word[0] not in hs_keywords]
+        word for word in similar_words if word[0] not in hs_keywords]
 
-    hs_related_words = [word for word in contextual_representation["related_words"] if word[
-        0] in hs_keywords] if contextual_representation["related_words"] else None
+    hs_related_words = [word for word in related_words if word[
+        0] in hs_keywords] if related_words else None
 
-    other_related_words = [word for word in contextual_representation["related_words"] if word[
-        0] not in hs_keywords] if contextual_representation["related_words"] else None
+    other_related_words = [word for word in related_words if word[
+        0] not in hs_keywords] if related_words else None
 
-    hs_similar_words_unbiased = [word for word in contextual_representation["similar_words_unbiased"] if word[
-        0] in hs_keywords] if contextual_representation["similar_words_unbiased"] else None
+    hs_similar_words_unbiased = [word for word in similar_words_unbiased if word[
+        0] in hs_keywords] if similar_words_unbiased else None
 
-    other_similar_words_unbiased = [word for word in contextual_representation["similar_words_unbiased"] if word[
-        0] not in hs_keywords] if contextual_representation["similar_words_unbiased"] else None
+    other_similar_words_unbiased = [word for word in similar_words_unbiased if word[
+        0] not in hs_keywords] if similar_words_unbiased else None
 
-    hs_related_words_unbiased = [word for word in contextual_representation["related_words_unbiased"] if word[
-        0] in hs_keywords] if contextual_representation["related_words_unbiased"] else None
+    hs_related_words_unbiased = [word for word in related_words_unbiased if word[
+        0] in hs_keywords] if related_words_unbiased else None
 
-    other_related_words_unbiased = [word for word in contextual_representation["related_words_unbiased"] if word[
-        0] not in hs_keywords] if contextual_representation["related_words_unbiased"] else None
+    other_related_words_unbiased = [word for word in related_words_unbiased if word[
+        0] not in hs_keywords] if related_words_unbiased else None
 
-    data = {"biased_freq": [biased_vocab[token]] if token in biased_vocab else [0],
-            "unbiased_freq": [unbiased_vocab[token]] if token in unbiased_vocab else [0],
+    data = {"biased_freq": [biased_vocab_freq[token]] if token in biased_vocab_freq else [0],
+            "unbiased_freq": [unbiased_vocab_freq[token]] if token in unbiased_vocab_freq else [0],
+
+            "biased_idf": [biased_vocab_idf[token]] if token in biased_vocab_idf else [0],
+            "unbiased_idf": [biased_vocab_idf[token]] if token in unbiased_vocab_idf else [0],
 
             "sim_hs_supp": compute_avg_cosine(hs_similar_words),
             "rel_hs_supp": compute_avg_cosine(hs_related_words) if hs_related_words else [],
 
-            "hs_sim_words": [word[0] for word in hs_similar_words] if hs_similar_words else [],
-            "alt_sim_words": [word[0] for word in other_similar_words] if other_similar_words else [],
+            "biased_sim_p@k": round(float(len(hs_similar_words)) / float(topn), 3),
+            "biased_rel_p@k": round(float(len(hs_related_words)) / float(topn), 3),
 
-            "hs_rel_words": [word[0] for word in hs_related_words] if hs_related_words else [],
-            "alt_rel_words": [word[0] for word in other_related_words] if other_related_words else [],
+            "ubiased_sim_p@k": round(float(len(hs_similar_words_unbiased)) / float(topn), 3),
+            "ubiased_rel_p@k": round(float(len(hs_related_words_unbiased)) / float(topn), 3),
 
-            "hs_sim_words_unbiased": [word[0] for word in hs_similar_words_unbiased] if hs_similar_words_unbiased else [],
-            "alt_sim_words_unbiased": [word[0] for word in other_similar_words_unbiased] if other_similar_words_unbiased else [],
+            "hs_sim_words": [word[0] for word in hs_similar_words
+                             ] if hs_similar_words else [],
 
-            "hs_rel_words_unbiased": [word[0] for word in hs_related_words_unbiased] if hs_related_words_unbiased else [],
-            "alt_rel_words_unbiased": [word[0] for word in other_related_words_unbiased] if other_related_words_unbiased else []}
+            "alt_sim_words": [word[0] for word in other_similar_words
+                              ] if other_similar_words else [],
+
+            "hs_rel_words": [word[0] for word in hs_related_words
+                             ] if hs_related_words else [],
+
+            "alt_rel_words": [word[0] for word in other_related_words
+                              ]if other_related_words else [],
+
+            "hs_sim_words_unbiased": [word[0] for word in hs_similar_words_unbiased
+                                      ] if hs_similar_words_unbiased else [],
+
+            "alt_sim_words_unbiased": [word[0] for word in other_similar_words_unbiased
+                                       ] if other_similar_words_unbiased else [],
+
+            "hs_rel_words_unbiased": [word[0] for word in hs_related_words_unbiased
+                                      ] if hs_related_words_unbiased else [],
+
+            "alt_rel_words_unbiased": [word[0] for word in other_related_words_unbiased
+                                       ] if other_related_words_unbiased else []}
     return data
 
 
 def compute_avg_cosine(similarity_result):
     """ Compute and return the average cosine similarities.
-    Args:
+    Args
+    ----
+
         similarity_result (list): List of word:cosine similarity dict pairs.
     Return:
         avg_cosine (float): Computed average cosine similarity
@@ -169,29 +258,36 @@ def compute_avg_cosine(similarity_result):
     return [avg_cosine]
 
 
-def get_contextual_representation(word, biased_embeddings, unbiased_embeddings, topn=5):
+def get_contextual_representation(**kwargs):
     """ Given a word we attempt to create a contextual representation that consists of
     semantically related words, semantically similar words and the avg word vector.
     We compute this from both a biased an unbiased embedding. By bias we refer to the
     method in which the data was originally collected, unbiased means that we streamed from
     Twitter with no keyword arguments.
+    Args
+    ----
 
-    Args:
         word (string): Target word.
+
         biased_embeddings (list): Stores (gensim.models): Gensim word embedding model
             0: dep2vec_embedding
             1: word_embedding (either fasttext or w2v)
+
         unbiased_embeddings (list): Stores (gensim.models): Gensim word embedding model
             0: base_dep2vec_embedding
             1: base_word_embedding (either fasttext or w2v)
-        topn (int): Number of words to check against in the embedding model.
+
+        topn (int): Number of words to check against in the embedding model, default=5.
     """
 
-    dep2vec_embedding = biased_embeddings[0]
-    word_embedding = biased_embeddings[1]
+    word = kwargs["token"]
+    topn = 5 if "topn" not in kwargs else kwargs["topn"]
 
-    base_dep2vec_embedding = unbiased_embeddings[0]
-    base_word_embedding = unbiased_embeddings[1]
+    dep2vec_embedding = kwargs["biased_embeddings"][0]
+    word_embedding = kwargs["biased_embeddings"][1]
+
+    base_dep2vec_embedding = kwargs["unbiased_embeddings"][0]
+    base_word_embedding = kwargs["unbiased_embeddings"][1]
 
     contextual_representation = {}
     contextual_representation["similar_words"] = dep2vec_embedding.similar_by_word(
